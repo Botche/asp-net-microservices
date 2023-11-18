ce2d36aaf65c66986cc033faa04f7a05687bcd50
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-18 17:24:32 +0200

    Create Discount Repository

---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Discount.Grpc.Repositories
+{
+    using Discount.Grpc.Entities;
+
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscountAsync(string productName);
+
+        Task<bool> CreateDiscountAsync(Coupon coupon);
+
+        Task<bool> UpdateDiscountAsync(Coupon coupon);
+
+        Task<bool> DeleteDiscountAsync(string productName);
+    }
+}
