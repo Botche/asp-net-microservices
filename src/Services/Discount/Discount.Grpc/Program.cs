bde62a0cc0c7ee8c6258cc9e9ce93bbbd13cf06b
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-18 17:50:09 +0200

    Create Discount Service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,5 @@
 using Discount.Grpc.Extensions;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,7 @@ builder.Services.MigrateDatabase<Program>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,55 @@
+﻿namespace Discount.Grpc.Services
+{
+    using System.Threading.Tasks;
+
+    using Discount.Grpc.Entities;
+    using Discount.Grpc.Protos;
+    using Discount.Grpc.Repositories;
+
+    using global::Grpc.Core;
+
+    using static Discount.Grpc.Protos.DiscountProtoService;
+
+    public class DiscountService : DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository discountRepository;
+        private readonly ILogger logger;
+
+        public DiscountService(IDiscountRepository discountRepository, ILogger logger)
+        {
+            this.discountRepository = discountRepository;
+            this.logger = logger;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, global::Grpc.Core.ServerCallContext context)
+        {
+            Coupon coupon = await this.discountRepository.GetDiscountAsync(request.ProductName);
+
+            if (coupon == null)
+            {
+                string statusMessage = $"Discount with ProductName = {request.ProductName} is not found.";
+                Status status = new(StatusCode.NotFound, statusMessage);
+                throw new RpcException(status);
+            }
+
+            CouponModel couponModel = mapper.Map<CouponModel>(coupon);
+
+            return couponModel;
+        }
+
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, global::Grpc.Core.ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, global::Grpc.Core.ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, global::Grpc.Core.ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
