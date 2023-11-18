b1d5e6bbd9aff18b9c7c711f3e48a6622fbcf292
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-18 09:30:39 +0200

    Implement Basket Repository

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace Basket.API.Repositories
+{
+    using System.Text.Json;
+
+    using Basket.API.Entities;
+
+    using Microsoft.Extensions.Caching.Distributed;
+
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            this.redisCache = redisCache;
+        }
+
+        public async Task DeleteBasketAsync(string userName)
+        {
+            await this.redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string userName)
+        {
+            var basket = await this.redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket)
+        {
+            var basketAsJson = JsonSerializer.Serialize(basket);
+            await this.redisCache.SetStringAsync(basket.UserName, basketAsJson);
+
+            return await GetBasketAsync(basket.UserName);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Repositories
+{
+    using Basket.API.Entities;
+
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasketAsync(string userName);
+
+        Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket);
+
+        Task DeleteBasketAsync(string userName);
+    }
+}
