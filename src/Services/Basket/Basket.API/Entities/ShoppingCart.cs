878e83eb456167ce910d2612aedfc2e27428bc55
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-18 09:11:49 +0200

    Create Basket entities

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            this.Items = new List<ShoppingCartItem>();
+        }
+
+        public ShoppingCart(string userName)
+            : this()
+        {
+            this.UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+        public IEnumerable<ShoppingCartItem> Items { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
