c129c227a83054949d586446f0d2334fabf880ca
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-18 15:53:36 +0200

    Create DiscountController.cs

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,57 @@
+﻿namespace Discount.API.Controllers
+{
+    using System.Net;
+
+    using Discount.API.Entities;
+    using Discount.API.Repositories;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository discountRepository;
+
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            this.discountRepository = discountRepository;
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            Coupon coupon = await this.discountRepository.GetDiscountAsync(productName);
+
+            return this.Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await this.discountRepository.CreateDiscountAsync(coupon);
+
+            return this.CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            bool result = await this.discountRepository.UpdateDiscountAsync(coupon);
+
+            return this.Ok(result);
+        }
+
+        [HttpDelete("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+        {
+            bool result = await this.discountRepository.DeleteDiscountAsync(productName);
+
+            return this.Ok(result);
+        }
+    }
+}
