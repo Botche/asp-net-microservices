2c8c5777803d51535ec84a54c0226da9a270da7e
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 20:13:14 +0200

    Create UnhandledExceptionBehaviour.cs

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,31 @@
+﻿namespace Ordering.Application.Behaviours
+{
+    using MediatR;
+
+    using Microsoft.Extensions.Logging;
+
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                string requestName = typeof(TRequest).Name;
+                this.logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+
+                throw;
+            }
+        }
+    }
+}
