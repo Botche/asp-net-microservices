e58f39ded54b5b9ef814740df400a768ca32d2a9
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 19:59:31 +0200

    Create ValidationException.cs

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException()
+            : base("One or more validation failures have occured.")
+        {
+            this.Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            : this()
+        {
+            this.Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+
+        public IDictionary<string, string[]> Errors { get; }
+    }
+}
