0993219ab43e3087f191edc7965f84afecc29794
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 20:26:08 +0200

    Add Dependency Injection extension method in Application layer

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿namespace Ordering.Application
+{
+    using System.Reflection;
+
+    using FluentValidation;
+
+    using MediatR;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using Ordering.Application.Behaviours;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            services.AddAutoMapper(executingAssembly);
+            services.AddValidatorsFromAssembly(executingAssembly);
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(executingAssembly));
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
