ee32bed858c9da50e8f9ee5bcf6b2dae9b64f9d3
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 19:49:46 +0200

    Implement DeleteOrderCommand

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using MediatR;
+
+    using Microsoft.Extensions.Logging;
+
+    using Ordering.Application.Contracts.Persistence;
+    using Ordering.Domain.Entities;
+
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly IMapper mapper;
+        private readonly ILogger<DeleteOrderCommand> logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleteOrderCommand> logger)
+        {
+            this.orderRepository = orderRepository;
+            this.mapper = mapper;
+            this.logger = logger;
+        }
+
+        public async Task Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            Order orderToDelete = await this.orderRepository.GetByIdAsync(request.Id);
+            if (orderToDelete == null)
+            {
+                this.logger.LogError("Order not exist on database.");
+                // throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            await this.orderRepository.DeleteAsync(orderToDelete);
+        }
+    }
+}
