539e88507c6048389145c048094e88c29f6c10ee
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 19:35:01 +0200

    Create CheckoutOrderCommandValidator.cs

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    using FluentValidation;
+
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            this.RuleFor(o => o.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characteres.");
+
+            this.RuleFor(p => p.EmailAddress)
+               .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            this.RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+        }
+    }
+}
