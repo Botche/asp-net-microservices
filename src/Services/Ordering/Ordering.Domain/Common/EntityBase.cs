1cbdb52d843ec2a1bb05c9de42f5119d0a65d3f6
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 18:18:04 +0200

    Add Common files

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.Common
+{
+    using System;
+
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
