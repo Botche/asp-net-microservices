4b3deb16651b6d97918be8c100507e0169d4d9df
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 18:28:23 +0200

    Create IOrderRepository.cs

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Contracts.Persistence
+{
+    using Ordering.Domain.Entities;
+
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
