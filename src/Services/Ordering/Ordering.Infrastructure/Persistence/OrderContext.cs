91b5f39cf20b5407a841149c1ac4ba57d54ab7ac
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 20:49:21 +0200

    Create OrderContext.cs

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,37 @@
+﻿namespace Ordering.Infrastructure.Persistence
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Ordering.Domain.Common;
+    using Ordering.Domain.Entities;
+
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "swn";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "swn";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
