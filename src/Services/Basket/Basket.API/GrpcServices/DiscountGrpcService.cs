5a36507427b0ce5493a9f20e282e7f7a73f998bd
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 08:58:21 +0200

    Encapsulate Discount gRPC service

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.GrpcServices
+{
+    using Discount.Grpc.Protos;
+
+    using static Discount.Grpc.Protos.DiscountProtoService;
+
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoServiceClient discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoServiceClient discountProtoService)
+        {
+            this.discountProtoService = discountProtoService;
+        }
+
+        public async Task<CouponModel> GetDiscountAsync(string productModel)
+        {
+            GetDiscountRequest discountRequest = new()
+            {
+                ProductName = productModel,
+            };
+
+            return await this.discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
