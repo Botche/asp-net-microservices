00bb5e272f124aa7e6a03b8c45c3e209da557373
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-19 20:53:51 +0200

    Create OrderContextSeed.cs

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,37 @@
+﻿namespace Ordering.Infrastructure.Persistence
+{
+    using Microsoft.Extensions.Logging;
+
+    using Ordering.Domain.Entities;
+
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+        {
+            if (!context.Orders.Any())
+            {
+                context.Orders.AddRange(GetPreconfiguredOrders());
+                await context.SaveChangesAsync();
+
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new()
+                {
+                    UserName = "swn",
+                    FirstName = "Mehmet",
+                    LastName = "Ozkaya",
+                    EmailAddress = "ezozkme@gmail.com",
+                    AddressLine = "Bahcelievler",
+                    Country = "Turkey",
+                    TotalPrice = 350
+                },
+            };
+        }
+    }
+}
