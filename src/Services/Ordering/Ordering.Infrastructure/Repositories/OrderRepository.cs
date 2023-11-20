d675c2a5980e2d69cd8692f226f73c34d835f39d
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-20 09:42:05 +0200

    Create OrderRepository.cs

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Ordering.Infrastructure.Repositories
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+
+    using Ordering.Application.Contracts.Persistence;
+    using Ordering.Domain.Entities;
+    using Ordering.Infrastructure.Persistence;
+
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext context) 
+            : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            return await this.Context.Orders
+                .Where(o => o.UserName == userName)
+                .ToListAsync();
+        }
+    }
+}
