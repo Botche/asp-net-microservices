e386fd96109085e68c46c2c3079c2b45f32a8275
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-20 10:34:48 +0200

    Create DatabaseMigrationExtensions for Ordering Database

---
+++ src/Services/Ordering/Ordering.API/Extensions/DatabaseMigrationExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace Discount.API.Extensions
+{
+    using Microsoft.Data.SqlClient;
+    using Microsoft.EntityFrameworkCore;
+
+    public static class DatabaseMigrationExtensions
+    {
+        public static WebApplication MigrateDatabase<TContext>(this WebApplication app,
+            Action<TContext, IServiceProvider> seeder,
+            int? retry = 0)
+            where TContext : DbContext
+        {
+            var logger = app.Services.GetRequiredService<ILogger<TContext>>();
+            var context = app.Services.GetService<TContext>();
+            
+            try
+            {
+                logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+
+                InvokeSeeder(seeder, context, app.Services);
+
+                logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+            }
+            catch (SqlException ex)
+            {
+                logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+
+                int retryForAvailability = retry.Value;
+                if (retryForAvailability < 50)
+                {
+                    retryForAvailability++;
+                    Thread.Sleep(2000);
+                    app.MigrateDatabase(seeder, retryForAvailability);
+                }
+            }
+
+            return app;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder,
+            TContext context,
+            IServiceProvider serviceProvider)
+            where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, serviceProvider);
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,8 @@
+using Discount.API.Extensions;
+
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,14 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    ILogger<OrderContextSeed> logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed
+        .SeedAsync(context, logger)
+        .Wait();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
