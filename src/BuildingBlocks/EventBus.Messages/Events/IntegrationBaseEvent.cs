1c58540600ebbb3fc55d13f9b486a1da2a895afb
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-21 22:02:13 +0200

    Create IntegrationBaseEvent.cs

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime creationDate)
+        {
+            this.Id = id;
+            this.CreationDate = creationDate;
+        }
+
+        public Guid Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
