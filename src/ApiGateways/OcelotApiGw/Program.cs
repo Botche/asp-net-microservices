9fc86cdeb5344cc246208264c1b5e28a3dd06237
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-22 16:42:18 +0200

    Setup Ocelot configuration

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -3,6 +3,9 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
 builder.Logging.AddConfiguration(builder.Configuration);
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
