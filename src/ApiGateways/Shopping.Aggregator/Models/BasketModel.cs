03a42df5bc48eaca542c16367967cb1c94001798
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-22 19:42:33 +0200

    Create Models for Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketModel
+    {
+        public BasketModel()
+        {
+            this.Items = new List<BasketItemExtendedModel>();
+        }
+
+        public string UserName { get; set; }
+
+        public IEnumerable<BasketItemExtendedModel> Items { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
