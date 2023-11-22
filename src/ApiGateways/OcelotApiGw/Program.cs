5aa95dfbd46ba87735e9a9396db3cea663c39949
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-22 16:16:58 +0200

    Register Ocelot Api Gateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,10 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Logging.AddConfiguration(builder.Configuration);
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
