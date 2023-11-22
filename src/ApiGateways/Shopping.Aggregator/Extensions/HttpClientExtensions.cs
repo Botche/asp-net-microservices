9d827c81a95f8cd917ebf25e2d65d5016c836751
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-22 20:01:03 +0200

    Create HttpClientExtensions.cs

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Shopping.Aggregator.Extensions
+{
+    using System.Text.Json;
+
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+            }
+
+            string dataAsString = await response.Content
+                .ReadAsStringAsync()
+                .ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
