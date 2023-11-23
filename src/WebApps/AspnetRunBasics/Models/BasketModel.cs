852f96202e573265a8b16023885cca273765fa84
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-23 11:24:01 +0200

    Create Models

---
+++ src/WebApps/AspnetRunBasics/Models/BasketModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AspnetRunBasics.Models
+{
+    public class BasketModel
+    {
+        public BasketModel()
+        {
+            this.Items = new List<BasketItemModel>();
+        }
+
+        public string UserName { get; set; }
+        public IEnumerable<BasketItemModel> Items { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
