93ec3febbe912d836a05b71b0e6ee2cb54c299fb
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-25 23:49:35 +0200

    Use Breadcrumb class instead of Tuple

---
+++ src/WebApps/AspnetRunBasics/Common/Breadcrumb.cs
@@ -0,0 +1,24 @@
+﻿namespace AspnetRunBasics.Common
+{
+    public class Breadcrumb
+    {
+        public Breadcrumb(string name)
+        {
+            this.Page = string.Empty;
+            this.Name = name;
+        }
+
+        public Breadcrumb(string page, string name)
+            : this(name)
+        {
+            this.Page = page;
+        }
+
+        public string Page { get; set; }
+
+        public string Name { get; set; }
+
+        public bool ShouldAppedPage => this.Page != string.Empty;
+
+    }
+}
